702bdeacbb31501bcf219e7770830d0585557805
Yago Coutinho <yagogc.it@gmail.com>
2022-11-24 23:01:08 -0300

    Exemplos de streams

---
+++ bin/dart_streams.dart
@@ -0,0 +1,5 @@
+import 'package:dart_streams/dart_streams.dart' as dart_streams;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_streams.calculate()}!');
+}
---
+++ test/dart_streams_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_streams/dart_streams.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
